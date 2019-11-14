89d1b6bad722e2c5fddef80521446a640085b017
Denise Williams <denise.marie.84@gmail.com>
2019-11-13 19:25:14 -0600

    studio complete

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,40 +1,50 @@
+import { Payload } from './Payload';
 import { Astronaut } from "./Astronaut";
 import { Cargo } from './Cargo';
 
 export class Rocket {
+        totalCapacityKg: number;
+        name: string;
+        astronauts: Astronaut [] = [];
+        cargoItems: Cargo [] = [];;
+        
         constructor(name: string, totalCapacityKg: number) {
            this.totalCapacityKg = totalCapacityKg;
            this.name = name;
 
 
         };
- }
-
- interface Payload {
-     let name: string;
-     let totalCapacityKg: number;
-     let cargoItems: Cargo[] = [];
-     let astronauts: Astronaut[] = []
- };
- 
- function sumMass( items: Payload[] ): number{
-     return this.massKg +=
- }
-
- function currentMassKg(): number{
-     return this.sumMass +=
- }
-
- function canAdd(item: Payload): boolean {
-     if (this.currentMassKg() + item.massKg <= this.totalCapacityKg){
-         return true;
-     }
- }
-
- function addCargo(cargo: Cargo) {
-
- }
-
- function addAstronaut(astronaut: Astronaut){
-
- }
+        sumMass (items: Payload[]): number {
+            let sum: number = 0;
+            for (let i=0; i < items.length; i++) {
+                sum += items[i].massKg;
+            }
+            return sum;
+        }
+    
+        currentMassKg (): number {
+            return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        }
+    
+        canAdd(item: Payload): boolean {
+            return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
+        }
+    
+        addCargo(cargo: Cargo): boolean {
+            if (this.canAdd(cargo)) {
+                this.cargoItems.push(cargo);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    
+        addAstronaut(astronaut: Astronaut): boolean {
+            if (this.canAdd(astronaut)) {
+                this.astronauts.push(astronaut);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
